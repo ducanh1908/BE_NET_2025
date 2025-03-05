de40b6535971a58b2e94ac09ac4943951a5591b1
AnhLee <ducanh.le1908@gmail.com>
2025-03-05 21:58:32 +0700

    create sessions 1

--- BE_NET_Sessions1/Program.cs
+++ BE_NET_Sessions1/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+         static int Tong(int a, int b)
+        {
+            return a + b;
+        }
+        static int Tich(int a, int b)
+        {
+            return a * b;
+        }
+        static int Hieu(int a, int b)
+        {
+            return a - b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Nhap so thu nhat: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so thu hai: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int Tong2So = Tong(num1, num2);
+            int Tich2So = Tich(num1, num2);
+            int Hieu2So = Hieu(num1, num2);
+
+            Console.WriteLine("Tong cua 2 so la:" +Tong2So );
+            Console.WriteLine("Tich cua 2 so la:" + Tich2So);
+            Console.WriteLine("Hieu cua 2 so la:" + Hieu2So);
         }
     }
 }
